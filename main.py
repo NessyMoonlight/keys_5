ddf463b29a1524f2bb35904cc77658ea26730e1c
Barbostridvaraz <borodinas@mer.ci.nsu.ru>
2024-04-09 18:47:53 +0700

    first

--- main.py
+++ main.py
@@ -1,44 +1,59 @@
 from tkinter import *
 from tkinter import messagebox
-class ted():
-   def __init__ (self, inventory, thrust, hunger, hp, max_inventory, max_thrust, max_hunger, max_hp):
-       self.inventory = inventory
-       self.thrust = thrust
-       self.hunger = hunger
-       self.hp = hp
-       self.max_inventory = 0
-       self.max_thrust = 4
-       self.max_hunger = 4
-       self.max_hp = 3
+import nachalo as nach
 
-t=ted(0,4,4,3,0,0,0,0)
+class chel():
+   def __init__(self,idpers):
+       self.idpers = idpers
+       self.inventory = 0
+       self.thrust = 4
+       self.hunger = 4
+       self.hp = 3
+       self.max_hp = 3 
+
+
+class room():
+    def __init__(self):
+        self.water = 0
+        self.food = 0
+        self.knife = 0
+
+Room = room()
+
+Ted = chel()
+
+Dolores = chel() # объект класса
+
+Timmi = chel()
+
+Mary = chel() # объект класса
 
 root = Tk()
 def btn_click():
-    hp = t.hp
-    inventory = t.inventory
-    hunger = t.hunger
-    thrust = t.thrust
+    hp = Ted.hp
+    inventory = Ted.inventory
+    hunger = Ted.hunger
+    thrust = Ted.thrust
     info = f"{str(hp)} ted's hp, {str(inventory)} ted's inventory, {str(hunger)} ted's, {str(thrust)} ted's"
-    messagebox.showinfo(title="ted", message=info)
+    messagebox.showerror(title="ted", message=info)
 def btn1_click():
-    if t.hp > 0:
-        t.hp -= 1
+    if Ted.hp > 0:
+        Ted.hp -= 1
 
 
 root.title("Игра")
 root.geometry("300x250")
 
-canvas = Canvas(root, width=300, height=250)
-canvas.pack()
+#canvas = Canvas(root, width=300, height=250)
+#canvas.pack()
 
-frame = Frame(root, bg="white")
+frame = Frame(root, bg="black")
 frame.place(relx=0, rely=0, relwidth=1, relheight=1)
 
 btn = Button(frame, text="карточка Теда", bg="red", command=btn_click)
 btn.pack()
 
-btn1 = Button(frame, text="карточка Теда", bg="red", command=btn1_click)
+btn1 = Button(frame, text="-hp", bg="red", command=btn1_click)
 btn1.pack()
 
 
